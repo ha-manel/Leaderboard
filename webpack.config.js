247ae7c86938260b2dadf8ea4e5f45abf3581438
Manel <manelhammouche3@gmail.com>
2022-05-11 12:56:32 +0100

    Update dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -26,12 +26,7 @@ module.exports = {
       {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
-      },
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: ['babel-loader'],
-      },
+      }
     ],
   },
 };
